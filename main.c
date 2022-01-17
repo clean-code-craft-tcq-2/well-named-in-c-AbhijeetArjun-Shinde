a9984f2b63aa76071a172efb5b987e81b099dbf8
AbhijeetArjun-Shinde <91106940+AbhijeetArjun-Shinde@users.noreply.github.com>
2022-01-17 09:55:36 +0530

    Update main.c

--- main.c
+++ main.c
@@ -4,11 +4,7 @@
 #include "Colors.h"
 #include "numberbycolor.h"
 
-void ColorPairToString(const ColorPair* colorPair, char* buffer) {
-    sprintf(buffer, "%s %s",
-        MajorColorNames[colorPair->majorColor],
-        MinorColorNames[colorPair->minorColor]);
-}
+
 
 
 
