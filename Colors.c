874359066858ecbb7f020a1751c6d8e6d83a3e6c
AbhijeetArjun-Shinde <91106940+AbhijeetArjun-Shinde@users.noreply.github.com>
2022-01-17 09:56:13 +0530

    Create Colors.c

---
+++ Colors.c
@@ -0,0 +1,7 @@
+#include "Colors.h"
+
+void ColorPairToString(const ColorPair* colorPair, char* buffer) {
+    sprintf(buffer, "%s %s",
+        MajorColorNames[colorPair->majorColor],
+        MinorColorNames[colorPair->minorColor]);
+}
