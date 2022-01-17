bc43b070e5ad4e793d54190480a35daf89821722
AbhijeetArjun-Shinde <91106940+AbhijeetArjun-Shinde@users.noreply.github.com>
2022-01-17 16:34:09 +0530

    Create PrintColorCodingManual.c

---
+++ PrintColorCodingManual.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "Colors.h"
+#include "GetColorByPairNumber.h"
+#include "GetPairNumberByColor.h"
+
+void ColorCodingManual()
+{
+	printf("\t 25 Pair Color Coding Manual \t");
+	printf(" \n Format: MajorColor MinorColor & Pair Number\n");
+	int pairNumber = 1 ;
+	while (pairNumber <= 25)
+	{
+		ColorPair colorPair = GetColorFromPairNumber(pairNumber);
+		char colorPairNames[MAX_COLORPAIR_NAME_CHARS];
+		ColorPairToString(&colorPair, colorPairNames);
+		printf("%s --> %d\n", colorPairNames, pairNumber);
+    pairNumber++
+	}
+}
